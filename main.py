5bd3acc8fcbb96389f152aec8aa6eac2fe80ddb9
Xianyi Liang <liangxianyi2011@gmail.com>
2024-08-01 15:38:00 -0400

    add columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns([2, 3])
@@ -14,3 +15,25 @@ Semper cubilia hac parturient ornare elit duis elit praesent ullamcorper. Eros s
 Dis sed phasellus mus ligula tortor egestas. Fames potenti efficitur tempus cursus est; arcu sollicitudin dictum. Congue nam quis; efficitur magna potenti imperdiet.
 """
     st.info(content)
+
+st.write(
+    "Below you can find some of the apps I have built with python. Feel free to contact."
+)
+
+df = pd.read_csv("data.csv", sep=";")
+
+col3, col4 = st.columns(2)
+with col3:
+    for _, row in df[0::2].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row["url"]})")
+
+with col4:
+    for _, row in df[1::2].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row["url"]})")
+
